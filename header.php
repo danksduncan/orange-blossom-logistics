d5926df5027abfc1770b9fecb1e0c6b9aa8a21d2
Duncan Li <58372639+danksduncan@users.noreply.github.com>
2023-01-21 14:25:31 -0500

    First Commit
    Moved the header into a separate PHP file for all pages on website excluding login and register pages.

---
+++ header.php
@@ -0,0 +1,35 @@
+<header id="header" class="header default fullWidth">
+    
+<div class="menu" id="onepagenav">  
+   <nav id="menu" class="mega-menu">
+    <section class="menu-list-items" style="background-color: black ;" >
+     <div class="container-fluid"> 
+      <div class="row"> 
+       <div class="col-lg-12 col-md-12">
+        <ul class="menu-logo">
+            <li>
+                <a href="https://orangeblossomlogistics.com"><img src="images/about/oblogo5.png" alt=""> </a>
+            </li>
+        </ul>
+        <div class="menu-bar">
+         <ul class="menu-links">
+
+			<li><a href="https://orangeblossomlogistics.com/">Home</a></li>
+			
+			<li><a href="https://orangeblossomlogistics.com/quote">Shippers</a></li>
+             
+ 			<li><a href="https://orangeblossomlogistics.com/carriers">Carriers</a></li>
+ 			
+            <li><a href="https://orangeblossomlogistics.com/about-us">About Us</a></li>
+            
+            <li><a href="https://orangeblossomlogistics.com/contact">Contact Us</a></li>
+        </ul>
+        
+        </div>
+       </div>
+      </div>
+     </div>
+    </section>
+   </nav>
+ </div>
+</header>
